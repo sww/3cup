83f835ea81429d7f129ee96d28c53accdbcda57f
Sean Wang <seanw@patreon.com>
2016-04-26 18:54:22 -0700

    Added the shot analysis.

---
+++ 3cup/analyze.py
@@ -0,0 +1,65 @@
+from models import Player, Shot, session
+
+
+def makes_for_name(name):
+    """Gets the make percentage per shot number for a person."""
+    print('Make percentage for {}'.format(name))
+
+    query = session.query(Shot).join(Player).filter(Player.name == name)
+
+    for i in range(1, 13):
+        attempts = query.filter(Shot.shot_number == i) \
+                        .count()
+
+        makes = query.filter(Shot.shot_number == i) \
+                     .filter(Shot.points > 0) \
+                     .count()
+        make_percentage = (makes / float(attempts or 1)) * 100
+        print('  Shot #{}: {:.2f}% ({}/{})'.format(i, make_percentage, makes, attempts))
+
+
+def non_moneyball_makes_for_name(name):
+    """Gets the non moneyball make percentage per shot number for a person."""
+    print('Non moneyball make percentage for {}'.format(name))
+
+    query = session.query(Shot).join(Player).filter(Player.name == name) \
+                                            .filter(Shot.is_moneyball.is_(False))
+
+    for i in range(1, 13):
+        attempts = query.filter(Shot.shot_number == i) \
+                        .count()
+
+        makes = query.filter(Shot.shot_number == i) \
+                     .filter(Shot.points > 0) \
+                     .count()
+        make_percentage = (makes / float(attempts or 1)) * 100
+        print('  Shot #{}: {:.2f}% ({}/{})'.format(i, make_percentage, makes, attempts))
+
+
+def moneyball_makes_for_name(name):
+    """Gets the moneyball make percentage per shot number for a person."""
+    print('Moneyball make percentage for {}'.format(name))
+
+    query = session.query(Shot).join(Player).filter(Player.name == name) \
+                                            .filter(Shot.is_moneyball.is_(True))
+
+    for i in range(1, 13):
+        moneyball_attempts = query.filter(Shot.shot_number == i) \
+                                  .count()
+
+        moneyball_makes = query.filter(Shot.shot_number == i) \
+                               .filter(Shot.points > 0) \
+                               .count()
+        moneyball_make_percentage = (moneyball_makes / float(moneyball_attempts or 1)) * 100
+        print('  Shot #{}: {:.2f}% ({}/{})'.format(i, moneyball_make_percentage, moneyball_makes, moneyball_attempts))
+
+
+if __name__ == '__main__':
+    import sys
+    name = sys.argv[1]
+    if not name:
+        return
+
+    makes_for_name(name)
+    non_moneyball_makes_for_name(name)
+    moneyball_makes_for_name(name)
